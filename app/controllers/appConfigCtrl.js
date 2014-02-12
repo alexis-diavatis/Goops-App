a7fa3b7f4163c3bbab86c7921bdc49b5fc716ffa
alexis diavatis <alexis.diavatis@gmail.com>
2014-02-12 17:32:48 +0200

    add a basic flow-control in syncing, so we can send notification when is done. rework on this and do it in parallel

--- app/controllers/appConfigCtrl.js
+++ app/controllers/appConfigCtrl.js
@@ -6,6 +6,8 @@ angular.module('ilgApp')
         var shellThemesDest = ilg_json.getHomePath() + '/.themes',
             gtkThemesDest = ilg_json.getHomePath() + '/.local/share/themes',
             iconThemesDest = ilg_json.getHomePath() + '/.local/share/icons';
+        var exec = require('child_process').exec,
+            child;
 
 
         $scope.appPath = configService.appPath();
@@ -32,8 +34,7 @@ angular.module('ilgApp')
 
         var openFolder = function (path) {
 
-            var exec = require('child_process').exec,
-                child;
+
             cmd = "gtk-launch nautilus " + path;
 
             child = exec(cmd, function (error, stdout, stderr) {
@@ -45,79 +46,101 @@ angular.module('ilgApp')
             });
 
         }
-       
-        
+
+
 
         /** Sync and create folders **/
-
+        //** This need to be hardly optimized! **//
         $scope.ilgAppSyncData = function () {
-
-            var exec = require('child_process').exec,
-                child;
-
-            /* Create directories if not exist, no need to check with stat */
 
             var shellThemesDest = ilg_json.getHomePath() + '/.themes',
                 gtkThemesDest = ilg_json.getHomePath() + '/.local/share/themes',
                 iconsThemesDest = ilg_json.getHomePath() + '/.local/share/icons';
 
-            fse.mkdirsSync(shellThemesDest);
-            fse.mkdirsSync(gtkThemesDest);
-            fse.mkdirsSync(iconThemesDest);
+            async.series([
+
+                function (callback) {
+
+                    var exec = require('child_process').exec,
+                        child;
+
+                    /* Create directories if not exist, no need to check with stat */
+
+                    fse.mkdirsSync(shellThemesDest);
+                    fse.mkdirsSync(gtkThemesDest);
+                    fse.mkdirsSync(iconThemesDest);
+
+                    callback(null, 'create paths');
+                },
+
+                function (callback) {
+                    /* COPY SHELL THEMES IN APPR FOLDERS ~/.themes */
+
+                    var shellThemesSrc = ilg_json.getDataPath() + 'shell/*';
+                    cmd = "cp -r " + shellThemesSrc + " " + shellThemesDest;
+
+                    child = exec(cmd, function (error, stdout, stderr) {
+                        console.log('stdout: ' + stdout);
+                        console.log('stderr: ' + stderr);
+                        if (error !== null) {
+                            console.log('exec error: ' + error);
+                        }
+                    });
+
+                    callback(null, 'copy shell themes');
+                },
+
+                function (callback) {
+                    /* COPY GTK THEMES IN APPR FOLDERS ~/.themes & ~/.local/share/themes/*/
+
+                    var gtkThemesSrc = ilg_json.getDataPath() + 'gtk/*';
+                    cmd = "cp -r " + gtkThemesSrc + " " + gtkThemesDest;
+                    child = exec(cmd, function (error, stdout, stderr) {
+                        console.log('stdout: ' + stdout);
+                        console.log('stderr: ' + stderr);
+                        if (error !== null) {
+                            console.log('exec error: ' + error);
+                        }
+                    });
+
+                    //Additionally copy inside themes for gtk2 support
 
 
-            /* COPY SHELL THEMES IN APPR FOLDERS ~/.themes */
+                    cmd = "cp -r " + gtkThemesSrc + " " + shellThemesDest;
+                    child = exec(cmd, function (error, stdout, stderr) {
+                        console.log('stdout: ' + stdout);
+                        console.log('stderr: ' + stderr);
+                        if (error !== null) {
+                            console.log('exec error: ' + error);
+                        }
+                    });
 
+                    callback(null, 'copy gtk themes');
+                },
 
-            var shellThemesSrc = ilg_json.getDataPath() + 'shell/*';
-            cmd = "cp -r " + shellThemesSrc + " " + shellThemesDest;
+                function (callback) {
+                    /* COPY Icons THEMES IN APPR FOLDERS  ~/.local/share/icons/ */
 
-            child = exec(cmd, function (error, stdout, stderr) {
-                console.log('stdout: ' + stdout);
-                console.log('stderr: ' + stderr);
-                if (error !== null) {
-                    console.log('exec error: ' + error);
+                    var iconsThemesSrc = ilg_json.getDataPath() + 'icons/*';
+
+                    cmd = "cp -r " + iconsThemesSrc + " " + iconsThemesDest;
+                    child = exec(cmd, function (error, stdout, stderr) {
+                        console.log('stdout: ' + stdout);
+                        console.log('stderr: ' + stderr);
+                        if (error !== null) {
+                            console.log('exec error: ' + error);
+                        }
+
+                    });
+
+                    callback(null, 'copy icons themes');
                 }
-            });
 
-            /* COPY GTK THEMES IN APPR FOLDERS ~/.themes & ~/.local/share/themes/*/
-
-            var gtkThemesSrc = ilg_json.getDataPath() + 'gtk/*';
-            cmd = "cp -r " + gtkThemesSrc + " " + gtkThemesDest;
-            child = exec(cmd, function (error, stdout, stderr) {
-                console.log('stdout: ' + stdout);
-                console.log('stderr: ' + stderr);
-                if (error !== null) {
-                    console.log('exec error: ' + error);
-                }
-            });
-
-            //Additionally copy inside themes for gtk2 support
-
-
-            cmd = "cp -r " + gtkThemesSrc + " " + shellThemesDest;
-            child = exec(cmd, function (error, stdout, stderr) {
-                console.log('stdout: ' + stdout);
-                console.log('stderr: ' + stderr);
-                if (error !== null) {
-                    console.log('exec error: ' + error);
-                }
-            });
-
-            /* COPY Icons THEMES IN APPR FOLDERS  ~/.local/share/icons/ */
-
-            var iconsThemesSrc = ilg_json.getDataPath() + 'icons/*';
-
-            cmd = "cp -r " + iconsThemesSrc + " " + iconsThemesDest;
-            child = exec(cmd, function (error, stdout, stderr) {
-                console.log('stdout: ' + stdout);
-                console.log('stderr: ' + stderr);
-                if (error !== null) {
-                    console.log('exec error: ' + error);
-                }
+            ], function (err, results) {
+                if (err) return console.error(err);
+                console.log(results);
 
             });
-
         }
 
 
