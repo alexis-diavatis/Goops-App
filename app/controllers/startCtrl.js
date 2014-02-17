156a658d71568224eae6e26ca379223bf2cf77bf
alexis diavatis <alexis.diavatis@gmail.com>
2014-02-17 18:40:57 +0200

    add a status icon

--- app/controllers/startCtrl.js
+++ app/controllers/startCtrl.js
@@ -2,6 +2,17 @@ angular.module('goopsApp')
 
 .controller('StartCtrl', ['$scope', 'configService', 'MsgService',
     function ($scope, configService, MsgService) {
-    
         
+        var tray;
+        
+        var addStatusIcon = function () {
+            tray = new gui.Tray({ title: 'Status Icon', tooltip: 'xaxa', icon: 'logo.png' });
+        }
+         
+        var removeStatusIcon = function () {
+            tray.remove();
+            tray=null;
+        }
+        addStatusIcon();
+       
 }]);
